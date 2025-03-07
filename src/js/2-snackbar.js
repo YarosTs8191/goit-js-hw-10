c9f6133fd7df2387d994e0279b2221e0ce67e8b6
YarosTs8191 <143120910+YarosTs8191@users.noreply.github.com>
2025-03-07 21:50:04 +0100

    sandbar-push

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,31 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+document.querySelector('.form').addEventListener('submit', event => {
+  event.preventDefault();
+
+  const delay = parseInt(event.target.elements.delay.value);
+  const state = event.target.elements.state.value;
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      state === 'fulfilled' ? resolve(delay) : reject(delay);
+    }, delay);
+  });
+
+  promise
+    .then(delay => {
+      iziToast.success({
+        title: '✅ Success',
+        message: `Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        title: '❌ Error',
+        message: `Rejected promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    });
+});
