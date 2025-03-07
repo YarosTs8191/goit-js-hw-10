3e6dd3c1402ce92a9155b493746821b260785aed
YarosTs8191 <143120910+YarosTs8191@users.noreply.github.com>
2025-03-07 22:19:51 +0100

    reset-comit

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -29,3 +29,15 @@ document.querySelector('.form').addEventListener('submit', event => {
       });
     });
 });
+
+document.body.insertAdjacentHTML(
+  'beforeend',
+  `
+  <nav>
+    <ul>
+      <li><a href="1-timer.html">Timer</a></li>
+      <li><a href="2-snackbar.html">Snackbar</a></li>
+    </ul>
+  </nav>
+`
+);
