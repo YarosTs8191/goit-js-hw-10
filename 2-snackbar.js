c395c2feba5339e1bc5e8a848c6990fa2302a4ed
YarosTs8191 <143120910+YarosTs8191@users.noreply.github.com>
2025-03-08 15:08:24 +0000

    Deploying to gh-pages from @ YarosTs8191/goit-js-hw-10@80c76e09d66861df86ddb4a25ac7eb4956320e39 🚀

--- 2-snackbar.js
+++ 2-snackbar.js
@@ -1,9 +1,2 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{i as s}from"./assets/vendor-BbbuE1sJ.js";document.querySelector(".form").addEventListener("submit",t=>{t.preventDefault();const i=parseInt(t.target.elements.delay.value),r=t.target.elements.state.value;new Promise((e,o)=>{setTimeout(()=>{r==="fulfilled"?e(i):o(i)},i)}).then(e=>{s.success({title:"✅ Success",message:`Fulfilled promise in ${e}ms`,position:"topRight"})}).catch(e=>{s.error({title:"❌ Error",message:`Rejected promise in ${e}ms`,position:"topRight"})})});document.body.insertAdjacentHTML("beforeend",`
-  <nav>
-    <ul>
-      <li><a href="1-timer.html">Timer</a></li>
-      <li><a href="2-snackbar.html">Snackbar</a></li>
-    </ul>
-  </nav>
-`);
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{i}from"./assets/vendor-BbbuE1sJ.js";document.querySelector(".form").addEventListener("submit",t=>{t.preventDefault();const s=parseInt(t.target.elements.delay.value),o=t.target.elements.state.value;new Promise((e,r)=>{setTimeout(()=>{o==="fulfilled"?e(s):r(s)},s)}).then(e=>{i.success({title:"✅ Success",message:`Fulfilled promise in ${e}ms`,position:"topRight"})}).catch(e=>{i.error({title:"❌ Error",message:`Rejected promise in ${e}ms`,position:"topRight"})})});
 //# sourceMappingURL=2-snackbar.js.map
